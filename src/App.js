272208bdb6237dc5b72caaea5e71e762a1e1c456
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2024-09-09 20:53:24 +0200

    more projects css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import projectlist from './projects.json';
 function App() {
   const list = projectlist.map((data, index) => (
     <div className='project' key={index}>
-      {data.title} 
+    <span className='project-title'> {data.title} </span> 
     </div>
   ));
 
@@ -14,7 +14,7 @@ function App() {
       <div className='mainpage'>
         <div className='title'>Hello, I'm David</div>
         <div className='undertitle'>I'm a Student With a Passion for Programming</div>
-        <button onClick={() => window.open('https://github.com', '_blank')}>GitHub</button>
+        <button onClick={() => window.open('https://github.com/KopnickyDavid', '_blank')}>GitHub</button>
       </div>
       <div className='projects'>
         <h1>My Projects</h1>
