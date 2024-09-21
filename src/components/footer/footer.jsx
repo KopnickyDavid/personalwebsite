5e5d598bbabc53050b8f81f4ce079aa42f9817f9
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2024-09-21 14:25:31 +0200

    links

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,6 @@
 import './footer.css';
-import { FaGithub ,FaLinkedin,FaInstagram ,FaTwitter  } from 'react-icons/fa';
+import { FaGithub ,FaLinkedin,FaInstagram    } from 'react-icons/fa';
+import { FaXTwitter } from "react-icons/fa6";
 import { IconContext } from "react-icons";
 
 function Footer() {
@@ -9,10 +10,10 @@ return (
           <p> Portfolio | &copy; Kopnicky 2024  </p>  
             <ul className="socials" >
                 <IconContext.Provider value={{ color: "white", className: "react-icons" }}>
-                    <li ><a href="#"><FaGithub /></a></li>
-                    <li ><a href="#"><FaLinkedin/></a></li>
-                    <li ><a href="#"><FaInstagram /></a></li>
-                    <li ><a href="#"><FaTwitter  /></a></li>
+                    <li ><a href="https://github.com/KopnickyDavid"><FaGithub /></a></li>
+                    <li ><a href="https://www.linkedin.com/in/kopnicky-david/"><FaLinkedin/></a></li>
+                    <li ><a href="https://www.instagram.com/kopnickydavid/"><FaInstagram /></a></li>
+                    <li ><a href="https://x.com/KopnickyD11681"><FaXTwitter    /></a></li>
                 </IconContext.Provider>
             </ul>
         </div>
