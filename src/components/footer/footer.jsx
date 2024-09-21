bc9791944bb47089b9aa8cd50699108125bb228d
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2024-09-21 15:23:59 +0200

    footer fix

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,16 +6,17 @@ import { IconContext } from "react-icons";
 function Footer() {
 return (  
     <footer>
-        <div className="footer-content" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <p> Portfolio | &copy; Kopnicky 2024  </p>  
+        <div className="footer-content" >
             <ul className="socials" >
                 <IconContext.Provider value={{ color: "white", className: "react-icons" }}>
                     <li ><a href="https://github.com/KopnickyDavid"><FaGithub /></a></li>
                     <li ><a href="https://www.linkedin.com/in/kopnicky-david/"><FaLinkedin/></a></li>
                     <li ><a href="https://www.instagram.com/kopnickydavid/"><FaInstagram /></a></li>
-                    <li ><a href="https://x.com/KopnickyD11681"><FaXTwitter    /></a></li>
+                    <li ><a href="https://x.com/KopnickyD11681"><FaXTwitter/></a></li>
                 </IconContext.Provider>
             </ul>
+            <p>Portfolio | &copy; Kopnicky 2024</p>  
+
         </div>
     </footer>
 );
