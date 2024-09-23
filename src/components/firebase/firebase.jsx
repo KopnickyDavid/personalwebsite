b748fda0daab4ce9ee62e8e5bb14522078a1fce7
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2024-09-23 22:26:10 +0200

    little mobile edit

--- src/components/firebase/firebase.jsx
+++ src/components/firebase/firebase.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -13,6 +14,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 async function getArticles() {
     const articles = [];
--- src/pages/mainpage/App.js
+++ src/pages/mainpage/App.js
@@ -19,26 +19,29 @@ function App() {
     </div>
   ));
  
+  const isMobile = window.innerWidth <= 768;
+
   return (
     <div className="App">
-          <style jsx global>{`body { margin: 0px;padding: 0px;}`}</style>
+      <style jsx global>{`body { margin: 0px;padding: 0px;}`}</style>
       <div className='mainpage'>
         <div className='title'>Hello, I'm David</div>
         <div className='undertitle'>I'm a Student With a Passion for Programming</div>
         <IconContext.Provider value={{ color: "white", className: "react-icons1" }}> 
           <button onClick={() => window.open('https://github.com/KopnickyDavid', '_blank')}> <FaGithub/> GitHub</button>
-      </IconContext.Provider>
+        </IconContext.Provider>
       </div>
-      <div className='picture'>
-      <img src={main}></img>
-      </div>
+      {!isMobile && (
+        <div className='picture'>
+          <img src={main} alt="Main"></img>
+        </div>
+      )}
       <div className='about' id='aboutme'>
         <h1>About me</h1>
-        <p><img src={me}></img>
+        <p><img src={me} alt="Me"></img>
         I’m a high school student with a strong passion for programming that has been with me since a young age. Over the years, I’ve developed a keen interest in building digital solutions, focusing specifically on front-end development. My primary goal is to create engaging, user-friendly interfaces that make an impact. I also work with Python, which has helped me broaden my understanding of programming concepts and problem-solving techniques.
         Currently, I’m in the process of mastering React, one of the most in-demand JavaScript libraries for front-end development. With each project I take on, I aim to refine my skills and push the boundaries of what I can create. Once I’ve honed my React skills, I’m excited to move on to learning Node.js to further enhance my backend knowledge and become a more versatile developer.
         Outside of coding, I’m always looking for ways to stay active, whether it’s through sports or other physical activities. I believe that balancing both mental and physical challenges is important for growth and helps keep me motivated and focused. Programming isn’t just a hobby for me; it’s a way to constantly evolve and learn, and I’m always looking for opportunities to improve my skills and contribute to meaningful projects.</p>
-
       </div>
       <Skills/>  
       <ArticlesPage/>
